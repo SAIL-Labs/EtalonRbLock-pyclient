8f923f5b6002c537909e0e1ad3a2e013b9fdd76a
Chris Betters <chris@chrisbetters.com>
2018-04-09 10:00:45 +1000

    - smarter ip selection

--- app/baseconfig.py
+++ app/baseconfig.py
@@ -2,18 +2,20 @@ import os
 import datetime
 import socket
 import numpy as np
+import platform
+
 
 class erblconfig:
+    def __init__(self, * args, ** kwargs):
+        if platform.uname().node is 'red-pitaya':
+            self.rpIP = socket.gethostbyname('localhost')
+        else:
+            self.rpIP = socket.gethostbyname(
+                'redpitaya1.sail-laboratories.com')
+            # self.rpIP = '192.168.1.100'
+            # self.rpIP = '10.66.100.233'
+            # self.rpIP = '10.42.0.249
 
-    def __init__(self, *args, **kwargs):
-<<<<<<< HEAD
-        #self.rpIP = socket.gethostbyname('redpitaya1.sail-laboratories.com')
-        #self.rpIP = '192.168.1.100'
-	#self.rpIP = socket.gethostbyname('redpitaya1.sail-laboratories.com')
-        #self.rpIP = '192.168.1.100'
-        #self.rpIP = '10.66.100.233'
-        #self.rpIP = '10.42.0.249'
-        self.rpIP = socket.gethostbyname('localhost')
         self.localIP = getLocalInterfaceIp(self.rpIP)
 
         self.arduinoport = '/dev/ttyUSB0'
@@ -23,12 +25,12 @@ class erblconfig:
               0.995185223418579],
              [0.00240740227660535, 0.00240740227660535, 0, 1, -0.995185195446789, 0]])
 
-        #self.samplescale_nm = 9.1604852488056226e-07  # nm per sample, 20hz, 20000 samples, DE_64, sweep amp 20, centre
-        #self.samplescale_nm = 1.0914981908744188e-06
+        # self.samplescale_nm = 9.1604852488056226e-07  # nm per sample, 20hz, 20000 samples, DE_64, sweep amp 20, centre
+        # self.samplescale_nm = 1.0914981908744188e-06
         self.samplescale_ms = 0.526
 
         self.decimation = 64
-        #self.aquisitionsize = 1280000 // self.decimation # 20000
+        # self.aquisitionsize = 1280000 // self.decimation # 20000
         self.aquisitionsize = 1536000 // self.decimation  # 24000
 
         self.degperms = 1 / 8200
@@ -46,22 +48,21 @@ class erblconfig:
             self.Ti = 250
             self.Td = 0
 
-        self.baseDirectory = os.path.join(os.getcwd(),'data')
+        self.baseDirectory = os.path.join(os.getcwd(), 'data')
         os.makedirs(self.baseDirectory, exist_ok=True)
-        self.image_directory = os.path.join(self.baseDirectory, 'specimages', datetime.date.today().isoformat())
+        self.image_directory = os.path.join(
+            self.baseDirectory, 'specimages', datetime.date.today().isoformat())
         os.makedirs(self.image_directory, exist_ok=True)
-        self.rbLockData_directory = os.path.join(self.baseDirectory, 'rbData', datetime.date.today().isoformat())
+        self.rbLockData_directory = os.path.join(
+            self.baseDirectory, 'rbData', datetime.date.today().isoformat())
         os.makedirs(self.rbLockData_directory, exist_ok=True)
-
-
-
 
 
 def getLocalInterfaceIp(rpIP='8.8.8.8'):
 
     localIP = ([l for l in (
-            [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [
-                [(s.connect((rpIP, 53)), s.getsockname()[0], s.close()) for s in
-                 [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+        [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [
+            [(s.connect((rpIP, 53)), s.getsockname()[0], s.close()) for s in
+             [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
 
     return localIP
